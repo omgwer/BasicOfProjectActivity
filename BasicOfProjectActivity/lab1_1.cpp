21e35c4fce0286081ab6def11d481c483859fd81
Alexandr Kislitsyn <63747048+omgwer@users.noreply.github.com>
2022-09-13 20:34:33 +0300

    feat: добавил первую лабу

--- BasicOfProjectActivity/lab1_1.cpp
+++ BasicOfProjectActivity/lab1_1.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std; 
 
 int main()
 {
-    cout << "Hello Alexandr!" << endl;
+   /* cout << "Hello Alexandr!" << endl;
     puts("Hello, world!");
+
+    int a, b, c = 0;
+
+    printf("input 3 numbers - ");
+    scanf_s("%d %d %d", &a, &b, &c);
+    printf("result %d \n", a + b + c);
+
+    */
+    int a = 0, b = 0, c = 0;
+
+    printf("input 3 numbers for square - ");
+    scanf_s("%d %d %d", &a, &b, &c);
+
+    int d = b * b - 4 * a * c;
+    cout << d << endl;
+    if (d <  0) {
+        cout << "No root" << endl;
+    }
+    else if (d == 0) {
+        cout << "Root - " << ( -b / (2 * a) ) << endl;
+    }
+    else {
+        cout << "Root 1 - " << ( ( -b + std::sqrt(d)) / (2 * a)) << endl;
+        cout << "Root 2 - " << ( ( -b - std::sqrt(d)) / (2 * a)) << endl;
+    }
 }
---
+++ lab1_3/lab1_3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c = 0;
+    
+    scanf_s("%d %d %d", &a, &b, &c);
+    printf("result %d", a + b + c);
+}
