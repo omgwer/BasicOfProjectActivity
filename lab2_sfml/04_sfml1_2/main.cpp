2df28ff3881e055088ac26959534f60efc7d8ab5
aleksandr <hersgory2134@gmail.com>
2022-10-25 22:20:57 +0300

    feat: Add new lab 1.2

--- lab2_sfml/04_sfml1_2/main.cpp
+++ lab2_sfml/04_sfml1_2/main.cpp
@@ -4,24 +4,57 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Name");
+    sf::RenderWindow window(sf::VideoMode({1000, 500}), "Name");
     window.clear();
 
     sf::RectangleShape shape1;
-    shape1.setSize({180, 580});
+    shape1.setSize({25, 480});
     shape1.setPosition({10, 10});
-    shape1.setFillColor(sf::Color(0x4D, 0x4D, 0x4D));
+    shape1.setFillColor(sf::Color(0xFF, 0x0, 0x0));
     window.draw(shape1);
 
-    sf::CircleShape shape2(85);
-    shape2.setPosition({15, 30});
-    shape2.setFillColor(sf::Color(0x0, 0xBB, 0x0));
+    sf::RectangleShape shape2;
+    shape2.setSize({25, 270});
+    shape2.setRotation(210);
+    shape2.setPosition({35, 260});
+    shape2.setFillColor(sf::Color(0xFF, 0x0, 0x0));
     window.draw(shape2);
 
-    sf::CircleShape shape3(85);
-    shape3.setPosition({15, 220});
-    shape3.setFillColor(sf::Color(0xFF, 0xFF, 0x0));
+    sf::RectangleShape shape3;
+    shape3.setSize({25, 270});
+    shape3.setRotation(-30);
+    shape3.setPosition({25, 250});
+    shape3.setFillColor(sf::Color(0xFF, 0x0, 0x0));
     window.draw(shape3);
+
+    sf::RectangleShape shape4;
+    shape4.setSize({25, 470});
+    shape4.setRotation(10);
+    shape4.setPosition({300, 20});
+    shape4.setFillColor(sf::Color(0xFF, 0x0, 0x0));
+    window.draw(shape4);
+
+    sf::RectangleShape shape5;
+    shape5.setSize({25, 470});
+    shape5.setRotation(-10);
+    shape5.setPosition({310, 20});
+    shape5.setFillColor(sf::Color(0xFF, 0x0, 0x0));
+    window.draw(shape5);
+
+    sf::RectangleShape shape6;
+    shape6.setSize({25, 100});
+    shape6.setRotation(90);
+    shape6.setPosition({360, 250});
+    shape6.setFillColor(sf::Color(0xFF, 0x0, 0x0));
+    window.draw(shape6);
+
+    sf::CircleShape shape7;
+    shape7.setRadius(200);
+    shape7.setPosition({500, 50});
+    shape7.setFillColor(sf::Color(150, 150, 150));
+    shape7.setOutlineThickness(10);
+    shape7.setOutlineColor(sf::Color(250, 0, 0));
+    window.draw(shape7);
 
     window.display();
 
--- lab2_sfml/04_sfml1_2/start.bat
+++ lab2_sfml/04_sfml1_2/start.bat
@@ -1,5 +1,12 @@
+if exist 04_sfml1_2.exe (
+    del 04_sfml1_2.exe
+)
+
 cd ..
     cmake --build .
 cd 04_sfml1_2
+
+if exist 04_sfml1_2.exe (
     start 04_sfml1_2.exe
-cd ..
+)
+    
