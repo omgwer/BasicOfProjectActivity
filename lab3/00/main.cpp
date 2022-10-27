1cfa21740ebd4138b14b93928ac53121b15e866b
aleksandr <hersgory2134@gmail.com>
2022-10-27 21:32:55 +0300

    feat: add first task lab3

---
+++ lab3/00/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    for (int num = 1; num <= 100l; num++)
+    {
+        if ((num % 3 == 0) && (num % 5 == 0))
+        {
+            std::cout << "FizzBuzz" << std::endl;
+        }
+
+        else if (num % 3 == 0)
+        {
+            std::cout << "Fizz" << std::endl;
+        }
+
+        else if (num % 5 == 0)
+        {
+            std::cout << "Buzz" << std::endl;
+        }
+
+        else
+        {
+            std::cout << num << std::endl;
+        }
+    }
+}
