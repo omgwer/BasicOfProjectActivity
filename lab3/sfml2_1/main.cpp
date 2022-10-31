01c026cdc8e9c5c9f3ae8dfdd8b3880f74422cc1
aleksandr <hersgory2134@gmail.com>
2022-10-31 23:12:09 +0300

    refactor: Поправил задание 2.1

--- lab3/sfml2_1/main.cpp
+++ lab3/sfml2_1/main.cpp
@@ -5,19 +5,21 @@
 
 constexpr unsigned WINDOW_WIDTH = 800;
 constexpr unsigned WINDOW_HEIGHT = 600;
+constexpr float BALL_SIZE = 40.f;
+constexpr float OFFSET_Y = 90.f;  
+constexpr float SPEED_X = 0.0005;
+constexpr float amplitudeY = 80.f;
+constexpr float periodY = 2; 
 
 int main()
 {
-    constexpr float BALL_SIZE = 40;       
-
     sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Wave Moving ball");
     sf::Clock clock;
-    sf::Vector2f position = {50.f, 100.f};      
+    sf::Clock speedXSync;
+    sf::Vector2f position = {50.f, 250.f};    
     sf::CircleShape shape(BALL_SIZE);
-    int movingVector = 1;
-
     shape.setFillColor(sf::Color(254, 254, 254));
-
+    float speedX = 100.f; 
 
     while (window.isOpen())
     {
@@ -26,21 +28,25 @@ int main()
         {
             if (event.type == sf::Event::Closed)
             {
-                window.close();
+                window.close();            
             }
         }
 
-        constexpr float speedX = 100.f;
-        constexpr float amplitudeY = 80.f;
-        constexpr float periodY = 2;
-
-        const float time = clock.getElapsedTime().asSeconds();
+        float time = clock.getElapsedTime().asSeconds();
+        sf::Vector2f currentPosition = shape.getPosition();
         const float wavePhase = time * float(2 * M_PI);
-        const float x = speedX * time;
-        const float y = amplitudeY * std::sin(wavePhase / periodY);
-        const sf::Vector2f offset = {x, y};
 
-        shape.setPosition(position + offset * movingVector);
+        if ((position.x + 2 * BALL_SIZE >= WINDOW_WIDTH) && (speedX > 0)) {
+            speedX = -speedX;
+        }
+        if ((position.x < 0 ) && (speedX < 0)) {
+            speedX = -speedX;   
+        }    
+
+        position.y =  amplitudeY * std::sin(wavePhase / periodY) +  OFFSET_Y;
+        position.x = currentPosition.x + speedX * speedXSync.restart().asSeconds();       
+
+        shape.setPosition(position);
         window.clear();
         window.draw(shape);
         window.display();
