8b42ed72699b2282c42ad8c9504877616af0c869
Alexandr Kislitsyn <63747048+omgwer@users.noreply.github.com>
2022-11-06 15:27:02 +0300

    feat: add sfml3.1

---
+++ lab4/sfml3_1/main.cpp
@@ -0,0 +1,108 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <iostream>
+
+void init(sf::ConvexShape &pointer)
+{
+    pointer.setPointCount(3);
+    pointer.setPoint(0, {40, 0});
+    pointer.setPoint(1, {-20, -20});
+    pointer.setPoint(2, {-20, 20});
+    pointer.setPosition({400, 300});
+    pointer.setFillColor(sf::Color(254, 128, 0));
+}
+
+float toDegrees(float radians)
+{
+    return float(double(radians) * 180.0 / M_PI);
+}
+
+void onMouseMove(const sf::Event::MouseMoveEvent &event, sf::Vector2f &mousePosition)
+{
+    std::cout << "mouse x=" << event.x << " , y= " << event.y << std::endl;
+    mousePosition = {float(event.x), float(event.y)};
+}
+
+void pollEvents(sf::RenderWindow &window, sf::Vector2f &mousePosition)
+{
+    sf::Event event;
+
+    while (window.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case ::sf::Event::Closed:
+            window.close();
+            break;
+        case sf::Event::MouseMoved:
+            onMouseMove(event.mouseMove, mousePosition);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void update(const sf::Vector2f &mousePosition, sf::ConvexShape &pointer, const float dt)
+{
+    const sf::Vector2f delta = mousePosition - pointer.getPosition();
+    float angle = atan2(delta.y, delta.x);
+    const float rotationSpeed = 15 * dt;
+    const float currentAngleRotation = pointer.getRotation();
+    if (angle < 0)
+    {
+        angle += 2 * M_PI;
+    }
+    angle = toDegrees(angle);
+
+    const float rotation = abs(angle - currentAngleRotation);
+
+    if (angle != currentAngleRotation)
+    {
+        if (angle > currentAngleRotation)
+        {
+            if ((angle - 180) > currentAngleRotation)
+                pointer.setRotation(currentAngleRotation - std::min(rotationSpeed, rotation));
+            else
+                pointer.setRotation(currentAngleRotation + std::min(rotationSpeed, rotation));
+        }
+        else
+        {
+            if ((angle + 180) < currentAngleRotation)
+                pointer.setRotation(currentAngleRotation + std::min(rotationSpeed, rotation));
+            else
+                pointer.setRotation(currentAngleRotation - std::min(rotationSpeed, rotation));
+        }
+    }
+}
+
+void redrawFrame(sf::RenderWindow &window, sf::ConvexShape &pointer)
+{
+    window.clear();
+    window.draw(pointer);
+    window.display();
+}
+
+int main()
+{
+    constexpr unsigned WINDOW_WIDTH = 800;
+    constexpr unsigned WINDOW_HEIGHT = 600;
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Pointer follows mouse", sf::Style::Default, settings);
+    sf::Clock clock;
+
+    sf::ConvexShape pointer;
+    sf::Vector2f mousePosition;
+
+    init(pointer);
+
+    while (window.isOpen())
+    {
+        const float dt = clock.restart().asSeconds();
+        pollEvents(window, mousePosition);
+        update(mousePosition, pointer, dt);
+        redrawFrame(window, pointer);
+    }
+}
